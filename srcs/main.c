023d1dfa7d783378d32b492b35b8d55a6355cfa0
Alan Barnett <alanbarnett328@gmail.com>
2020-03-01 07:35:14 -0800

    chore(setup): struct for fdf data created

--- srcs/main.c
+++ srcs/main.c
@@ -6,12 +6,27 @@
 /*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/08 17:24:12 by alan              #+#    #+#             */
-/*   Updated: 2019/12/08 17:43:13 by alan             ###   ########.fr       */
+/*   Updated: 2020/03/01 07:29:44 by abarnett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_mem.h"
 #include "ft_printf.h"
+#include "mlx.h"
 #include <unistd.h>
+
+#define WIDTH 1000
+#define HEIGHT 800
+
+struct s_fdf
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+	void	*img_ptr;
+	char	*img_data;
+	int		img_bits_per_pixel;
+	int		img_size_line;
+};
 
 static void	show_usage(void)
 {
@@ -19,6 +34,32 @@ static void	show_usage(void)
 			"usage: fdf filename.fdf",
 			"Takes a single filename argument",
 			"For an example of an fdf file, see mini42.fdf");
+}
+
+/*
+** Creates the mlx connection, window, and image I will be using. Gets the data
+** about the image as well, so it is ready to use.
+*/
+
+static struct s_fdf	*fdf_setup()
+{
+	struct s_fdf	*data;
+	int				endian;
+
+	data = (struct s_fdf *)ft_memalloc(sizeof(struct s_fdf));
+	data->mlx_ptr = mlx_init();
+	data->win_ptr = mlx_new_window(data->mlx_ptr, WIDTH, HEIGHT, "fdf - abarnett");
+	data->img_ptr = mlx_new_image(data->mlx_ptr, WIDTH, HEIGHT);
+	data->img_data = mlx_get_data_addr(data->img_ptr, &(data->img_bits_per_pixel), &(data->img_size_line), &endian);
+	return (data);
+}
+
+void		fdf()
+{
+	struct s_fdf	*data;
+
+	data = fdf_setup();
+	mlx_loop(data->mlx_ptr);
 }
 
 int			main(int argc, char **argv)
@@ -31,5 +72,6 @@ int			main(int argc, char **argv)
 	{
 		show_usage();
 	}
+	fdf();
 	return (0);
 }
