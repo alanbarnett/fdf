3cc0d7491271ee52eae124a998798564470ccd93
Alan Barnett <alanbarnett328@gmail.com>
2020-03-18 04:48:26 -0700

    fix(plot_map_line): send in better coordinates
    Send in an x,y,z coordinate with the y axis pointing up, properly
    aligned in the center of the grid.

--- includes/draw.h
+++ includes/draw.h
@@ -6,14 +6,13 @@
 /*   By: abarnett <alanbarnett328@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/10 20:02:39 by abarnett          #+#    #+#             */
-/*   Updated: 2020/03/10 20:03:58 by abarnett         ###   ########.fr       */
+/*   Updated: 2020/03/17 04:10:00 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DRAW_H
 # define DRAW_H
 
-void	plot_map_line(struct s_fdf *data, int x1, int x2, int y1, int y2);
 void	plot_map(struct s_fdf *data);
 int		draw_image(struct s_fdf *data);
 
--- srcs/draw.c
+++ srcs/draw.c
@@ -6,7 +6,7 @@
 /*   By: abarnett <alanbarnett328@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/07 01:43:43 by abarnett          #+#    #+#             */
-/*   Updated: 2020/03/14 02:02:09 by alan             ###   ########.fr       */
+/*   Updated: 2020/03/18 04:12:02 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,28 +17,32 @@
 #include "mlx.h"
 
 /*
-** Draws a line from the map into the image, using parameters as coordinates
-** into the array. Shift coordinates by half the width and height of the map,
-** to center it in the coordinate plane. Then project it by scale to make it
-** larger.
+** Draws a line from the map into the image
+**
+** Sets the Z coordinate from the map, applying the scale
+** Shifts x and y coordinates to be centered on the origin
+** Sets Y to be positive up, instead of positive down (like the array)
+** Expands the points by scale
 */
 
-void		plot_map_line(struct s_fdf *data, int x1, int x2, int y1, int y2)
+static void	plot_map_line(struct s_fdf *data, int x1, int x2, int y1, int y2)
 {
 	struct s_point	start;
 	struct s_point	end;
 
 	start.z = data->map[y1][x1] * data->scale;
 	end.z = data->map[y2][x2] * data->scale;
-	x1 -= (data->map_width / 2);
-	x2 -= (data->map_width / 2);
-	y1 -= (data->map_height / 2);
-	y2 -= (data->map_height / 2);
-	start.x = x1 * data->scale;
-	start.y = y1 * data->scale;
-	end.x = x2 * data->scale;
-	end.y = y2 * data->scale;
-	fdf_plot_line(data, start, end);
+	start.x = x1 - (data->map_width / 2.0);
+	start.y = y1 - (data->map_height / 2.0);
+	end.x = x2 - (data->map_width / 2.0);
+	end.y = y2 - (data->map_height / 2.0);
+	start.y *= -1;
+	end.y *= -1;
+	start.x *= data->scale;
+	start.y *= data->scale;
+	end.x *= data->scale;
+	end.y *= data->scale;
+	fdf_plot_line(data, &start, &end);
 }
 
 /*
@@ -47,7 +51,7 @@ void		plot_map_line(struct s_fdf *data, int x1, int x2, int y1, int y2)
 
 void		plot_map(struct s_fdf *data)
 {
-	for (int i = 0; data->map[i]; ++i)
+	for (int i = 0; i < data->map_height; ++i)
 	{
 		for (int j = 0; j < data->map_width; ++j)
 		{
@@ -57,7 +61,7 @@ void		plot_map(struct s_fdf *data)
 				plot_map_line(data, j, j + 1, i, i);
 			}
 			// draw line down
-			if (data->map[i + 1])
+			if (i + 1 < data->map_height)
 			{
 				plot_map_line(data, j, j, i, i + 1);
 			}
