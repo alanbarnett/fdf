974558898cc8e94634edd6d1d8083829d1f308b7
Alan Barnett <alanbarnett328@gmail.com>
2020-04-28 10:55:19 -0700

    chore(settings): change config around a little

--- includes/config.h
+++ includes/config.h
@@ -6,7 +6,7 @@
 /*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/10 16:07:28 by alan              #+#    #+#             */
-/*   Updated: 2020/04/15 04:29:52 by alan             ###   ########.fr       */
+/*   Updated: 2020/04/28 06:42:06 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,27 @@
 */
 
 /*
+** The name of the window that gets created
+*/
+# define WINDOW_NAME "fdf - abarnett"
+
+/*
 ** The width and height of the window.
 ** These are used a fair amount in calculations as well, to ensure I don't draw
 ** outside the window and to center the camera.
 */
-# define WIDTH (800)
-# define HEIGHT (600)
+# define WIDTH (1000)
+# define HEIGHT (800)
 
 /*
 ** The base amount of rotation degrees per keypress
 */
-# define ROTATION_DEGREES (5)
+# define ROTATION_DEGREES (2)
 
 /*
 ** The amount of milliseconds for each frame while in the rotation function
 */
-# define ROTATION_SPEED_MS (40000)
+# define ROTATION_SPEED_MS (20000)
 
 /*
 ** Default scale (distance between points in the file)
@@ -45,6 +50,6 @@
 ** i.e. the value (1000) says that with a z of 1000, the projected x and y
 ** value should double
 */
-# define VANISHING_POINT (1000)
+# define VANISHING_POINT (8000)
 
 #endif
