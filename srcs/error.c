5eaaf700b99af128746578c7da11aefaac75bb13
Alan Barnett <alanbarnett328@gmail.com>
2020-05-01 06:21:42 -0700

    test(errors): error lock function, somewhat like errno

--- srcs/error.c
+++ srcs/error.c
@@ -6,13 +6,43 @@
 /*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/09 23:24:44 by alan              #+#    #+#             */
-/*   Updated: 2019/12/09 23:28:09 by alan             ###   ########.fr       */
+/*   Updated: 2020/01/13 21:05:45 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "error.h"
 #include "ft_printf.h"
 #include <unistd.h>
+
+/*
+** Stores the passed error code into the internal static error code. Calling
+** this function with an error code of 0 will return the error it was
+** holding, as well as resetting it to 0.
+*/
+
+static int			error_code_lock(enum e_err_code new_error)
+{
+	static enum e_err_code	error = 0;
+
+	if (new_error == 0)
+	{
+		new_error = error;
+		error = 0;
+		return (new_error);
+	}
+	error = new_error;
+	return (0);
+}
+
+void				set_error_code(enum e_err_code error_code)
+{
+	error_code_lock(error_code);
+}
+
+enum e_err_code		get_error_code()
+{
+	return (error_code_lock(0));
+}
 
 /*
 ** This function checks the error code and returns the string it corresponds to
@@ -37,11 +67,11 @@ static const char	*get_error_string(enum e_err_code error_code)
 			? (error_strings[error_code]) : 0);
 }
 
-int					print_error(enum e_err_code error_code)
+int					print_error()
 {
 	const char	*error_string;
 
-	error_string = get_error_string(error_code);
+	error_string = get_error_string(get_error_code());
 	if (error_string)
 	{
 		ft_printfd(STDERR_FILENO, "fdf: error: %s\n", error_string);
